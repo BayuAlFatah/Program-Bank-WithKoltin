7ea1dbe6eaa68f0a70bd0be6513e82c6bff64194
BayuAlFatah <bayualfatahsetiawan5@gmail.com>
2023-01-06 19:46:57 +0700

    menambahkan dan mengurangi sejumlah method di class main.kt  dan BankKotlin.Kt

--- BankKotlin.kt
+++ BankKotlin.kt
@@ -5,15 +5,23 @@ class BankKotlin(Name : String,Number : Long,Money : Int) {
     var number : Long = Number
     private var money : Int = Money
 
+        get() = field
+        set(value) {
+            field = value
+        }
 
     fun getInfo(){
         println("Your Name = $nama")
         println("your Number = $number")
         println("Your Money = $money")
     }
-    fun isiUlang(saldo : Int){
+    fun setor(saldo : Int){
         println("Isi Ulang Sejumlah $saldo")
         money += saldo
+    }
+    fun tarik(saldo : Int){
+        println("Tarik saldo sejumlah : $saldo")
+        money -= saldo
     }
     fun checkSaldo(){
         println("Your Money = $money")
--- main.kt
+++ main.kt
@@ -1,19 +1,73 @@
 package LatihanKotlin.`Bank-Kotlin`
 
+import java.util.Scanner
+
 fun main(){
-    var bank = BankKotlin("bayu",21441900,0)
+    var a : Boolean = true
+    var inputUser = Scanner(System.`in`)
+    println("Masukkan UserName anda : ")
+    var nama = inputUser.next()
+    println("Masukkan No Pin anda : ")
+    var number = inputUser.nextLong()
+    if (number.toInt() != 21441900){
+        println("No Pin salah Silahkan Masukkan No Pin yang benar")
+        main()
+    }
+    var bank = BankKotlin(nama,number,0)
     var bank1 = BankKotlin("eko",22334455,0)
+
+    while (a){
+        println("\n")
+        println("||----Selamat datang di Bank kami----||")
+        println("||---Silahkan Pilih menu Transaksi---||")
+        println("|| 1. Untuk Setor Tunai \t\t\t ||")
+        println("|| 2. Untuk Transaksi Lainnya \t\t ||")
+        println("|| 3. Untuk Keluar/Cancel \t\t ||")
+        println("||-----------------------------------||")
+        println("Ketikkan angka untuk Menu Transaksi : ")
+        var input = inputUser.nextInt()
+        if (input == 1){
+            println("masukkan Jumlah Setor tunai")
+            var setor = inputUser.nextInt()
+            bank.setor(setor)
+        }else if (input == 2){
+
+        }else{
+            println("Angka tidak Ditemukan")
+            println("Silahkan masukkan Username lagi")
+        }
+    }
+
+
     // untuk menampilkan nama,number,dan saldo
     bank.getInfo()
     //method untuk mengisi ulang saldo dimasukkan ke parameter
-    bank.isiUlang(10000)
+    bank.setor(100_000)
     // Method untuk mengecek saldo
     bank.checkSaldo()
+    //method untuk Tarik
+    bank.tarik(10_000)
     //method untuk bayar
     bank.bayar(5000)
     // method untuk Trasnfer
-    bank.transfer(bank1,2000)
+    bank.transfer(bank1,20_000)
 
     // cek ke bank1 apakah sudah ditransfer
     bank1.getInfo()
 }
+//fun yesorno(message : String) : Boolean{
+//    var inputUser = Scanner(System.`in`)
+//    println("$message 1/2")
+//    var hasil = inputUser.next()
+//
+//    while (!hasil.equals(1) && !hasil.equals(2) && !hasil.equals(3)){
+//        println("Pilihan Anda bukan 1/2/3")
+//        println("silahkan masukkan $message (1/2/3)")
+//        hasil = inputUser.next()
+//    }
+//    return hasil.equals()
+//}
+fun eksekusi(){
+
+}
+
